7bdb56526340a8b47dfb612e1952a6de51666d2a
Jaroslav Zaoral <zaoralj@gmail.com>
2018-10-09 21:48:30 +0200

    owin

--- OwinApp/Program.cs
+++ OwinApp/Program.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Owin.Hosting;
-using System;
-using System.Net.Http;
+﻿using Topshelf;
 
 namespace OwinApp
 {
@@ -8,20 +6,15 @@ namespace OwinApp
     {
         static void Main(string[] args)
         {
-            string baseAddress = "http://localhost:9000/";
-
-            // Start OWIN host 
-            using (WebApp.Start<Startup>(url: baseAddress))
+            var rc = HostFactory.Run(x =>
             {
-                // Create HttpCient and make a request to api/values 
-                HttpClient client = new HttpClient();
-
-                var response = client.GetAsync(baseAddress + "api/values").Result;
-
-                Console.WriteLine(response);
-                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
-                Console.ReadLine();
-            }
+                x.Service<Service>(s =>
+                {
+                    s.ConstructUsing(name => new Service());
+                    s.WhenStarted(service => service.Start());
+                    s.WhenStopped(service => service.Stop());
+                });
+            });
         }
     }
 }
---
+++ OwinApp/Service.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace OwinApp
+{
+    public class Service
+    {
+        private IDisposable _webServer;
+
+        public void Start()
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            _webServer = WebApp.Start<Startup>(url: baseAddress);
+        }
+
+        public void Stop()
+        {
+            _webServer?.Dispose();
+        }
+    }
+}
